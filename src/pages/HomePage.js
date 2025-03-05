c4dbad66e0b5ef7cb26020dc3f521e00a3d74a7b
VLoand <lb52070@ubt-uni.net>
2025-03-05 10:23:53 +0100

    Setup Complete

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Voting App</h1>
+      <p>Create and participate in polls and votes.</p>
+      <a href="/login">Login</a>
+    </div>
+  );
+};
+
+export default HomePage;
